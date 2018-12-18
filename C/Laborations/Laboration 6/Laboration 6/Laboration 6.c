ec38be5b7d179562efffe520108c11ad692f3cc2
samuelosteby <samuel.osteby96@hotmail.se>
2018-12-18 11:54:15 +0100

    Added the ability to add students and write it to the database

--- C/Laborations/Laboration 6/Laboration 6/Laboration 6.c
+++ C/Laborations/Laboration 6/Laboration 6/Laboration 6.c
@@ -2,30 +2,85 @@
 // Made by Hampus Tuokkola & Samuel Östeby
 
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
+#include <stdlib.h>
+#include <string.h> // !! EVALUATE IF NECESSARY !!
+#include <ctype.h> // !! EVALUATE IF NECESSARY !!
 
 struct Student
 {
 	int pnumber;
-	char name[30];
-	char gender[5];
-	char sprogram[30];
+	char name[50];
+	char gender[10];
+	char sprogram[50];
 	int age;
 	char email[50];
-
-	int *Student
 };
+
+struct Student AddAStudent()
+{
+	struct Student student;
+	printf("Choose a name: \n");
+	fgets(student.name, 50, stdin);
+	student.name[strlen(student.name) - 1] = '\0';
+	printf("Choose an age: \n");
+	scanf("%d", &student.age);
+	getchar();
+	printf("Choose a gender: \n");
+	fgets(student.gender, 10, stdin);
+
+	return student;
+}
+
+int fileexists(const char * filename) {
+	FILE *file;
+	if (file = fopen(filename, "r")) {
+		fclose(file);
+		return 1;
+	}
+	return 0;
+}
 
 int main()
 {
-	FILE *database;
-	int choice = 0, option = 0;
+	struct Student *students = NULL, student;
+	// Check if database exists. Create one if needed.
+	FILE *studentdbtest = fopen("studentdb.dat", "r");
+	if (studentdbtest == NULL)
+	{
+		printf("No database exists.\n");
+		printf("Creating a student database called 'studentdb.dat'.\n");
+	}
+	else
+	{
+		printf("Database located.\n");
+		fclose(studentdbtest);
+	}
+	FILE *studentdb = fopen("studentdb.dat", "wb+");
+	printf("Welcome to your student database. What would you like to do?\n");
+	printf("1. Add new student\n2. Modify existing student\n3. Delete student\n4. Search for student\n5. Save database\n6. Load database\n7. Exit\n");
+	int choice = 0, option = 0, toexit = 0;
+	scanf("%d", &option);
 	while (choice == 0)
 	{
 		switch (option)
 		{
 			case 1: // Add
+				int complete = 0;
+				while (complete == 0)
+				{
+					printf("Press 0 to exit. Press anything else to continue.\n");
+					scanf("%d", &toexit);
+					if (toexit == 0)
+					{
+						complete = 1;
+					}
+					getchar();
+					getchar();
+					student = AddAStudent();
+					fwrite(&student, sizeof(struct Student), 1, studentdb);
+					puts(student.name);
+				}
+				choice = 1;
 				break;
 			case 2: // Modify
 				break;
@@ -42,5 +97,6 @@ int main()
 				break;
 		}
 	}
+	printf("Goodbye!");
 	return 0;
 }
