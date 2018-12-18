e95f218eb92de278fe95edd927f9870f20ed4beb
samuelosteby <samuel.osteby96@hotmail.se>
2018-12-18 13:11:22 +0100

    Revert "Merge branch 'master' of https://github.com/samuelosteby/School-Programming"
    This reverts commit 1825811e49872467d1dd2bf1b1c4c46465718f0d, reversing
    changes made to b12f8bd35759b619580142f41003fcad3e89d81b.

--- C/Laborations/Laboration 5/Laboration 5/Laboration 5.c
+++ C/Laborations/Laboration 5/Laboration 5/Laboration 5.c
@@ -1,6 +1,3 @@
-// Programmering Grundkurs - Laboration 5
-// Made by Hampus Tuokkola & Samuel Östeby
-
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
@@ -63,6 +60,7 @@ void remove_character(char s[], char c) // Removes all entities of a character i
 		if (s[i] != c)
 		{
 			newstr[strlen(newstr)] = s[i];
+			printf("aids");
 		}
 	}
 
@@ -71,93 +69,7 @@ void remove_character(char s[], char c) // Removes all entities of a character i
 	return 0;
 }
 
-void add_character(char s[], char c) // Add all entities of a character to end of every word
-{
-	int i, lengthofstr;
-	char newstr[100] = "";
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if ((isspace(s[i])) || (s[i] == '\0'))
-		{
-			newstr[strlen(newstr)] = c;
-			newstr[strlen(newstr)] = s[i];
-		}
-		else
-		{
-			newstr[strlen(newstr)] = s[i];
-		}
-	}
 
-	printf("\n");
-	puts(newstr);
-	return 0;
-}
-
-void replace_character(char s[], char c, char r) // Replace all entities of a character with another character
-{
-	int i, lengthofstr;
-	char newstr[100] = "";
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == c)
-		{
-			newstr[strlen(newstr)] = r;
-		}
-		else
-		{
-			newstr[strlen(newstr)] = s[i];
-		}
-	}
-
-	printf("\n");
-	puts(newstr);
-	return 0;
-}
-
-void statistics_string(char s[]) // Count number of words, total number of characters and the number of each character in a string
-{
-	int i, j, lengthofstr, characters, words, charfrequency;
-	words = 1;
-	charfrequency = 0;
-	char newstr[100] = "", activechar, finishlist[100] = "";
-	printf("\n");
-
-	for (i = 0; s[i] != '\0'; i++) // Count the amount of words
-	{
-		if ((isspace(s[i])) && (isalnum(s[i+1])))
-		{
-			words += 1;
-		}
-		
-		newstr[strlen(newstr)] = s[i];
-	}
-
-	for (i = 0; s[i] != '\0'; i++) // Count the frequency of all characters
-	{
-		activechar = s[i];
-		for (j = 0; s[j] != '\0'; j++) // Go through each character and compare it to the rest of the string
-		{
-			if (s[j] == activechar)
-			{
-				charfrequency += 1;
-			}
-		}
-
-		if ((strchr(finishlist, activechar) == NULL) && (activechar != '\n'))
-		{
-			printf("The character %c occurs %d times.\n", activechar, charfrequency);
-			finishlist[strlen(finishlist)] = activechar;
-		}
-
-		charfrequency = 0;
-	}
-
-	characters = strlen(newstr) - 1; // The total amount of characters
-
-	printf("\n");
-	printf("The entered text contains %d words and %d characters in total.\n\n", words, characters);
-	return 0;
-}
 
 int main()
 { 
@@ -195,54 +107,34 @@ int main()
 			break;
 		case 4:
 			// Remove a character
-			char remove_char;
+			char replace_char;
 			printf("Enter text:\n");
 			fgets(inputstr, maxch - 1, stdin);
 			printf("Enter a character to remove:\n");
-			scanf("%c", &remove_char);
+			scanf("%c", &replace_char);
 			getchar();
-			remove_character(inputstr, remove_char);
+			remove_character(inputstr, replace_char);
 			break;
 		case 5:
 			// Add a character
-			char add_char;
 			printf("Enter text:\n");
-			fgets(inputstr, maxch - 1, stdin);
-			printf("Enter a character to add at end of all words:\n");
-			scanf("%c", &add_char);
-			getchar();
-			add_character(inputstr, add_char);
 			break;
 		case 6:
 			// Replace a character
-			char replace_char, replacement_char;
 			printf("Enter text:\n");
-			fgets(inputstr, maxch - 1, stdin);
-			printf("Enter a character to replace:\n");
-			scanf("%c", &replace_char);
-			getchar();
-			printf("Enter a character to act as the replacement:\n");
-			scanf("%c", &replacement_char);
-			getchar();
-			replace_character(inputstr, replace_char, replacement_char);
 			break;
 		case 7:
 			// Statistics
 			printf("Enter text:\n");
-			fgets(inputstr, maxch - 1, stdin);
-			statistics_string(inputstr);
 			break;
 		case 8:
 			// Exit
-			printf("Goodbye!\n\n");
-			finished = 1;
 			break;
 		default:
 			// Invalid option
 			break;
 		}
 	}
-	
-	system("pause");
+
 	return 0;
 }
