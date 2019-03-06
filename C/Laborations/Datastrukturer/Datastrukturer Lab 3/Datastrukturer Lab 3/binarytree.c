ee530c5ab6ea8368fe7f773b13c009ce3fb71982
samuelosteby <samuel.osteby96@hotmail.se>
2019-03-06 09:57:41 +0100

    Defined and traverse through trees in Lab3

--- C/Laborations/Datastrukturer/Datastrukturer Lab 3/Datastrukturer Lab 3/binarytree.c
+++ C/Laborations/Datastrukturer/Datastrukturer Lab 3/Datastrukturer Lab 3/binarytree.c
@@ -6,12 +6,20 @@
 
 Root * getNewRoot(Root *NewRoot, int data)
 {
-	NewRoot = (Root*)malloc(sizeof(Root));
-	NewRoot->data = data;
-	NewRoot->left = NULL;
-	NewRoot->right = NULL;
+	if (isEmpty(NewRoot))
+	{
+		NewRoot = (Root*)malloc(sizeof(Root));
+		NewRoot->data = data;
+		NewRoot->left = NULL;
+		NewRoot->right = NULL;
 
-	return NewRoot;
+		return NewRoot;
+	}
+	else
+	{
+		printf("Root already exists!\n\n");
+		return NULL;
+	}
 }
 
 TreeNode * getNewTreeNode(Root *Tree, int data)
@@ -24,47 +32,112 @@ TreeNode * getNewTreeNode(Root *Tree, int data)
 	return Node;
 }
 
-void traverseTreeInOrder(Root *Tree, int print)
+void traverseTreeInOrder(Root *Tree)
 {
 	if (Tree == NULL)
 	{
 		return;
 	}
 
-	traverseTreeInOrder(Tree->left, print);
-	if (print == TRUE)
-	{
-		printf("%d ", Tree->data);
-	}
-	traverseTreeInOrder(Tree->right, print);
+	traverseTreeInOrder(Tree->left);
+	printf("%d ", Tree->data);
+	traverseTreeInOrder(Tree->right);
 }
 
-void traverseTreePreOrder(Root *Tree, int print)
+void traverseTreePreOrder(Root *Tree)
 {
 	if (Tree == NULL)
 	{
 		return;
 	}
 
-	if (print == TRUE)
-	{
-		printf("%d ", Tree->data);
-	}
-	traverseTreePreOrder(Tree->left, print);
-	traverseTreePreOrder(Tree->right, print);
+	printf("%d ", Tree->data);
+	traverseTreePreOrder(Tree->left);
+	traverseTreePreOrder(Tree->right);
 }
 
-void traverseTreePostOrder(Root *Tree, int print)
+void traverseTreePostOrder(Root *Tree)
 {
 	if (Tree == NULL)
 	{
 		return;
 	}
 
-	traverseTreePostOrder(Tree->left, print);
-	traverseTreePostOrder(Tree->right, print);
-	if (print == TRUE)
+	traverseTreePostOrder(Tree->left);
+	traverseTreePostOrder(Tree->right);
+	printf("%d ", Tree->data);
+}
+
+int isEmpty(Root *Tree)
+{
+	if (Tree == NULL)
 	{
-		printf("%d ", Tree->data);
+		return TRUE;
 	}
+	else
+	{
+		return FALSE;
+	}
+}
+
+TreeNode * insert(Root *Tree, TreeNode *TreeNode)
+{
+	if (isEmpty(Tree))
+	{
+		Tree = TreeNode;
+		return Tree;
+	}
+	else
+	{
+		if (Tree->data >= TreeNode->data)
+		{
+			if (Tree->left == NULL)
+			{
+				Tree->left = TreeNode;
+				return TreeNode;
+			}
+			else
+			{
+				insert(Tree->left, TreeNode);
+				return NULL;
+			}
+		}
+		else
+		{
+			if (Tree->right == NULL)
+			{
+				Tree->right = TreeNode;
+				return TreeNode;
+			}
+			else
+			{
+				insert(Tree->right, TreeNode);
+				return NULL;
+			}
+		}
+	}
+}
+
+Root * search(Root *Tree, int data)
+{
+	if (isEmpty(Tree))
+	{
+		return NULL;
+	}
+	else if (Tree->data == data)
+	{
+		return Tree;
+	}
+
+	if (Tree->data < data)
+	{
+		return search(Tree->right, data);
+	}
+
+	return search(Tree->left, data);
+}
+
+TreeNode * delete(Root *Tree, TreeNode *TreeNode)
+{
+	printf("Ayy lmao\n\n");
 }
--- C/Laborations/Datastrukturer/Datastrukturer Lab 3/Datastrukturer Lab 3/binarytree.h
+++ C/Laborations/Datastrukturer/Datastrukturer Lab 3/Datastrukturer Lab 3/binarytree.h
@@ -3,6 +3,10 @@
 #define _traverseTreeInOrder_h
 #define _traverseTreePreOrder_h
 #define _traverseTreePostOrder_h
+#define _isEmpty_h
+#define _insert_h
+#define _search_h
+#define _delete_h
 #define TRUE 1
 #define FALSE 0
 
@@ -19,8 +23,16 @@ Root * getNewRoot(Root *NewTree, int data); // Creates a new binary tree root an
 
 TreeNode * getNewTreeNode(Root *Tree, int data); // Creates a new tree node, ready to be placed in a binary tree
 
-void traverseTreeInOrder(Root *Tree, int print); // Traverses a binary tree inorder. Use TRUE or FALSE to indicate whether to print values in nodes
+void traverseTreeInOrder(Root *Tree); // Traverses a binary tree inorder. Use TRUE or FALSE to indicate whether to print values in nodes
 
-void traverseTreePreOrder(Root *Tree, int print); // Traverses a binary tree preorder. Use TRUE or FALSE to indicate whether to print values in nodes
+void traverseTreePreOrder(Root *Tree); // Traverses a binary tree preorder. Use TRUE or FALSE to indicate whether to print values in nodes
 
-void traverseTreePostOrder(Root *Tree, int print); // Traverses a binary tree postorder. Use TRUE or FALSE to indicate whether to print values in nodes
+void traverseTreePostOrder(Root *Tree); // Traverses a binary tree postorder. Use TRUE or FALSE to indicate whether to print values in nodes
+
+int isEmpty(Root *Tree); // Returns true if binary tree is empty, false otherwise
+
+TreeNode * insert(Root *Tree, TreeNode *TreeNode); // Inserts a node into a binary tree and returns it, otherwise returns NULL
+
+TreeNode * search(Root *Tree, int data); // Returns a node with the given data if it exists, returns NULL otherwise
+
+TreeNode * delete(Root *Tree, TreeNode *TreeNode); // Deletes the specified node from the binary tree and returns it, returns NULL otherwise
