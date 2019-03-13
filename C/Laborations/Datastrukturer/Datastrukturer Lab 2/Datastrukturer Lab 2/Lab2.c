3a9fa2c7ab7d508f247fa2b996989169b1530091
samuelosteby <samuel.osteby96@hotmail.se>
2019-03-14 00:03:07 +0100

    Fixed Lab2

--- C/Laborations/Datastrukturer/Datastrukturer Lab 2/Datastrukturer Lab 2/Lab2.c
+++ C/Laborations/Datastrukturer/Datastrukturer Lab 2/Datastrukturer Lab 2/Lab2.c
@@ -18,6 +18,7 @@ int main()
 	Iarraylab1->data[2] = 5;
 	Iarraylab1->data[3] = 10;
 	Iarraylab1->data[4] = 8;
+	Iarraylab1->data[5] = 7;
 	
 
 	Intarray * Iarraylab2 = NULL;
@@ -32,6 +33,15 @@ int main()
 	Intarray * Iarray = NULL;
 	Iarray = getnewIntarray(Iarray, 10);
 	Iarray->data[0] = 9;
+	Iarray->data[1] = 6;
+	Iarray->data[2] = 0;
+	Iarray->data[3] = 7;
+	Iarray->data[4] = 1;
+	Iarray->data[5] = 0;
+	Iarray->data[6] = 0;
+	Iarray->data[7] = 1;
+	Iarray->data[8] = 3;
+	Iarray->data[9] = 9;
 	// Sort arrays...
 	printArray(Iarraylab1->data, Iarraylab1->size);
 	printf("Sorting with bubble sort...\n\n");
